4d95da84dbec23a2d2b9503721bb6ef99bbdc508
Rodrigo Campos <rodrigoca@microsoft.com>
2022-05-06 13:07:32 +0200

    userns-cve: General improvements

--- userns-cve/demo.sh
+++ userns-cve/demo.sh
@@ -3,19 +3,25 @@
 . $(dirname ${BASH_SOURCE})/../util.sh
 
 desc_type "Kubernetes 1.25 will support user namespaces (KEP 127)"
+desc_type "This video shows:"
+desc_type "	* A small introduction to user namespaces"
+desc_type "	* An exploit of CVE-2022-0492: Can containers escape?"
+desc_type "	* How user namespaces block this (and more) exploits"
+desc_type ""
 desc_type "Containers in k8s today use a network namespace, PID namespace, mount namespace, etc."
 desc_type "However, they are not using a user namespace"
 desc_type ""
-desc_type "A user namespace allows us to map users in the container to a different user on the host"
+desc_type "A user namespace allows us to map a user in the container to a different user on the host"
 desc_type "For example:"
 desc ""
 desc "          Container UID       Host UID"
 desc "            0 (root)           10000 (unprivileged) "
 desc ""
-desc_type "If they escape the container, they don't have root privileges on the host."
-desc_type "They are running as UID 10000 in the host "
+desc_type "In this example, the UID 0 (root) in the container is mapped to UID 10000 on the host"
+desc_type "If they escape the container, they don't have root privileges on the host"
+desc_type "They are running as UID 10000 in the host"
 desc_type "Their root permissions are limited to the user namespace"
-desc_type "The same applies for capabilities (only valid in the user namespace)"
+desc_type "Their capabilities too (only valid in the user namespace)"
 desc_type "There are some more things that change, for more details see:"
 desc      "		man 7 user_namespaces"
 desc ""
@@ -36,9 +42,9 @@ desc "				CVE-2022-0492: Can containers escape?"
 desc "					       Score: 7.8"
 desc "					       March 3, 2022"
 desc ""
-desc_type "We created a exploit of this CVE for Kubernetes."
+desc_type "We created an exploit of this CVE for Kubernetes"
 
-desc_type "Let's see how this exploit works in vanilla k8s 1.24."
+desc_type "Let's see how this exploit works in vanilla k8s 1.24"
 desc_type "Then, let's see what happens if we try to run the exploit when the pod uses user namespaces"
 desc_type ""
 run "kubectl get nodes"
@@ -67,10 +73,11 @@ function host_ps {
 }
 
 function cve_check {
-	desc "Let's see if the file was created"
+	desc_type "Let's see if the file was created"
 	run "ls -l /cve-2022-0492"
-	desc "Let's see the content of the file"
+	desc_type "Let's see the content of the file"
 	run "cat /cve-2022-0492"
+	desc_type "Is the sleep 123456 process running?"
 	host_ps "sleep 123456"
 }
 
@@ -82,11 +89,11 @@ run "kubectl apply -f $(relative cve-pod-host-users.yaml)"
 kwait
 desc_type "Let's see from the host as which user it is running"
 host_ps "sleep infinity"
-desc_type "It is running as root"
+desc_type "It is running as root on the host"
 
 run "cat $(relative cve-script)"
 desc_type "Look at the CVE script and the comments"
-run "sleep 10"
+run "sleep 20"
 desc_type "Let's upload the script to exploit the cve"
 run "kubectl cp $(relative cve-script) $krun_pod:/"
 desc_type "If the script runs successfully, it will create a file in / on the HOST"
@@ -94,8 +101,9 @@ krun "chmod +x /cve-script; ./cve-script"
 cve_check
 desc_type "Indeed it executes as root and created the file on /!"
 # scared emoji
-echo -e "\U1F631"
-prompt
+desc_type "\U1F631"
+#echo -e "\U1F631"
+#prompt
 
 run "kubectl delete pod $krun_pod"
 desc "Also, I'm running in the background:"
@@ -103,11 +111,12 @@ desc "sudo rm /cve-2022-0492"
 sudo rm -f /cve-2022-0492
 desc "sudo pkill -f \"sleep 12345\""
 sudo pkill -f "sleep 12345"
+desc ""
 
 krun_pod="cve-pod-userns"
 
 desc_type "Let's create a pod with user namespaces enabled now"
-desc_type "It is the same pod, but with the hostUsers=false field."
+desc_type "It is the same pod, but with the hostUsers=false field"
 desc_type "This enables user namespaces"
 run "cat $(relative cve-pod-userns.yaml)"
 run "kubectl apply -f $(relative cve-pod-userns.yaml)"
@@ -125,33 +134,37 @@ run "cat $(relative cve-script)"
 run "kubectl cp $(relative cve-script) $krun_pod:/"
 desc_type "Again, if the script runs successfully, it will create a file in /"
 
-desc_type "But mount, the first line of the script, will fail."
+desc_type "But mount, the first line of the script, will fail"
 krun "mount -t cgroup -o rdma cgroup /mnt/"
 
 desc_type "Let's then, run the script inside a nested userns where we can mount"
 desc_type "Let's see if the exploit works..."
 
 # Crossed fingers emoji
-echo -e "\U1F91E"
-prompt
+desc_type "\U1F91E"
+#echo -e "\U1F91E"
+#prompt
 
 krun "unshare -UrmC bash -x /cve-script"
-desc_type "Now the mount works, but the exploit is not able to write the release_agent"
-desc_type "Nor the notify_on_release and cgroups.procs"
-desc_type "It seems this didn't work, but let's check to be sure"
+desc_type "Take a few seconds to see that, while the mount works now, the exploit still seems to fail"
+run "sleep 15"
+desc_type "So, what happened?"
+desc_type "As we said, now the mount works. But the exploit is not able to write the release_agent"
+desc_type "This is, we couldn't register our script to be later executed as root"
+desc_type "We couldn't write the notify_on_release and cgroups.procs files either"
+desc_type "So, it seems the exploit didn't work. But let's check to be sure"
 cve_check
 # emoji party
-echo -e "\U1F389"
-prompt
+desc_type "\U1F389"
 
+run "kubectl delete pod $krun_pod"
 desc_type "The exploit doesn't work because the container doesn't run as root on the HOST"
 desc_type "The files can only be written by root on the HOST"
 desc_type "However, the container still thinks it is running as root"
-desc_type "Lot of unmodified images that run as root by default, will:"
-desc_type "  * continue to run fine if user namespaces is enabled"
+desc_type "User namespaces gives this powerful abstraction of users"
+desc_type "Lot of images that run as root will:"
+desc_type "  * continue to run fine and unmodified if user namespaces are enabled"
 desc_type "  * be protected, as they don't really run as root"
 desc ""
-desc_type "User namespaces gives this powerful abstraction of users"
 desc_type "Coming soon, to a k8s cluster near you :)"
 
-run "kubectl delete pod $krun_pod"
